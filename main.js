0be45a5444c02220db4e65d680c09fe4fe4e3362
Bekir - CDI <b.burakcebecii@gmail.com>
2022-04-21 14:47:42 +0200

    main.js changed

--- main.js
+++ main.js
@@ -1,6 +1,33 @@
-import './style.css'
+import "./style.scss";
+import axios from "axios";
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const url =
+  "https://raw.githubusercontent.com/graphql-compose/graphql-compose-examples/master/examples/northwind/data/json/employees.json";
+const appElem = document.querySelector("#app");
+
+const title = "<h1>Employees</h1>";
+appElem.innerHTML = title + "<div>Loading...</div>";
+
+setTimeout(() => {
+  (async () => {
+    // fetch
+    // const response = await fetch(url);
+    // const employees = await response.json();
+
+    // axios
+    const response = await axios.get(url);
+    const employees = response.data;
+
+    appElem.innerHTML =
+      title +
+      `
+  <div class="employees">
+      ${employees
+        .map((employee) => {
+          return `<div class="employee">${employee.firstName} ${employee.lastName}</div>`;
+        })
+        .join("")} 
+  </div>
+`;
+  })();
+}, 2000);
